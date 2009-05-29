002a05399d4c2814605fac46b24bee4d48e610ef
b.lindahl <b.lindahl@b0bc8688-3f32-11de-b232-599c1b3c59e8>
2009-05-29 20:36:55 +0000

    Created build file generating script for linux.

---
+++ trunk/build/generate_build_files_premake4.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+chmod +x ../tools/premake4
+../tools/premake4 --file=GASSSim4sln.lua --cc=gcc --os=linux gmake
+../tools/premake4 --file=CoreTest4sln.lua --cc=gcc --os=linux gmake
+
+#../tools/premake4 --file=GASSSim4sln.lua --cc=gcc --os=linux codeblocks
+#../tools/premake4 --file=CoreTest4sln.lua --cc=gcc --os=linux codeblocks
+
