3f06dffa5d045477a4dfe8076eb52b8921980d6b
b.lindahl <b.lindahl@b0bc8688-3f32-11de-b232-599c1b3c59e8>
2009-05-29 20:58:42 +0000

    Added menu so script generate either Code::Block or GNU Makefiles, whichever the user prefer

--- trunk/build/generate_build_files_premake4.sh
+++ trunk/build/generate_build_files_premake4.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 chmod +x ../tools/premake4
-../tools/premake4 --file=GASSSim4sln.lua --cc=gcc --os=linux gmake
-../tools/premake4 --file=CoreTest4sln.lua --cc=gcc --os=linux gmake
 
-#../tools/premake4 --file=GASSSim4sln.lua --cc=gcc --os=linux codeblocks
-#../tools/premake4 --file=CoreTest4sln.lua --cc=gcc --os=linux codeblocks
+echo "1) Code::Blocks"
+echo "2) GNU makefiles"
+#echo "c) clean"
+
+echo "What kind of build files do you want to generate?"
+read answer
+case $answer in
+    1) export target=codeblocks;;
+    2) export target=gmake;;
+#    c)    echo You chose c;;
+    *)    echo You did not chose 1 or 2 ;;
+esac
+
+
+../tools/premake4 --file=GASSSim4sln.lua --cc=gcc --os=linux $target
+../tools/premake4 --file=CoreTest4sln.lua --cc=gcc --os=linux $target
 
