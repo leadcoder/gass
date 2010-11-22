2a5dd1a72a12c88dc6521e9bc29f83adf2a3cef1
leadcoder <leadcoder@b0bc8688-3f32-11de-b232-599c1b3c59e8>
2010-11-22 21:33:08 +0000

    script build files added

---
+++ trunk/build/GASSPluginScript4.lua
@@ -0,0 +1,84 @@
+project "GASSPluginScript"
+	kind "SharedLib"
+	language "C++"
+	targetprefix "" -- exchange this with soname in some way?
+
+	files { "../source/Plugins/Script/**.cpp", "../source/Plugins/Script/**.cxx", "../source/Plugins/Script/**.h" }
+
+	targetdir ( "../lib/" .. _ACTION )
+
+	
+
+
+if (os.is("windows")) then
+
+	includedirs 
+	{ 
+		"../source",
+		"../dependencies/tinyxml",
+		"../dependencies/lua/include",
+		"$(BOOST_PATH)"
+
+	}
+	
+	libdirs 
+	{ 
+		"../lib/" .. _ACTION,
+		"../dependencies/tinyxml/lib",
+		"../dependencies/lua/lib/" .. _ACTION,
+		"$(BOOST_PATH)/lib"
+
+	}
+
+	defines { "WIN32", "_CRT_SECURE_NO_WARNINGS", "GASS_PLUGIN_EXPORTS" }
+	flags { "NoPCH", "No64BitChecks" } --, "NoRTTI" }
+	
+else
+	includedirs 
+	{ 
+		"../source",
+		"../dependencies/tinyxml",
+		"../dependencies/lua/include",
+		"$(BOOST_PATH)"
+
+	}
+
+	libdirs 
+	{ 
+		"../lib/" .. _ACTION,
+		"../dependencies/tinyxml/lib",
+		"../dependencies/lua/lib/" .. _ACTION,
+		"$(BOOST_PATH)/lib"
+
+	}
+
+end
+
+
+	configuration "Debug"
+		targetname "GASSPluginScript_d"
+		defines { "DEBUG" }
+		flags { "Symbols" }
+--		debugPrefix = "_d"
+		links 
+		{
+			"GASSCore_d",
+			"GASSSim_d",
+			"lua_d",
+			"tinyxmld"
+		}
+
+	configuration "Release"
+		targetname "GASSPluginScript"
+		defines { "NDEBUG" }
+		flags { "Optimize" }
+		links 
+		{
+			"GASSCore",
+			"GASSSim",
+			"lua",
+			"tinyxml"
+		}
+
+
+
