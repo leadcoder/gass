f725de990413ded003a69cd12341288f5cf2150e
leadcoder <leadcoder@b0bc8688-3f32-11de-b232-599c1b3c59e8>
2011-03-30 12:27:54 +0000

    simple box geometry helper component added

---
+++ trunk/source/Plugins/Base/Components/BoxGeometryComponent.cpp
@@ -0,0 +1,109 @@
+/****************************************************************************
+*                                                                           *
+* HiFiEngine                                                                *
+* Copyright (C)2003 - 2005 Johan Hedstrom                                   *
+* Email: hifiengine@gmail.com                                               *
+* Web page: http://n00b.dyndns.org/HiFiEngine                               *
+*                                                                           *
+* HiFiEngine is only used with knowledge from the author. This software     *
+* is not allowed to redistribute without permission from the author.        *
+* For further license information, please turn to the product home page or  *
+* contact author. Abuse against the HiFiEngine license is prohibited by law.*
+*                                                                           *
+*****************************************************************************/
+
+#include "BoxGeometryComponent.h"
+#include "Plugins/Base/CoreMessages.h"
+#include "Core/ComponentSystem/ComponentFactory.h"
+#include "Core/MessageSystem/MessageManager.h"
+#include "Core/MessageSystem/IMessage.h"
+#include "Core/Utils/Log.h"
+#include "Sim/Scenario/Scene/ScenarioScene.h"
+#include "Sim/Scenario/Scene/SceneObject.h"
+#include "Sim/SimEngine.h"
+#include "Sim/Components/Graphics/Geometry/IGeometryComponent.h"
+#include "Sim/Components/Graphics/MeshData.h"
+#include "Sim/Scenario/Scene/SceneObjectManager.h"
+#include "Sim/Scenario/Scene/SceneObjectMessages.h"
+
+
+namespace GASS
+{
+	BoxGeometryComponent::BoxGeometryComponent(void) : m_Size(1,1,1)
+	{
+
+	}
+
+	BoxGeometryComponent::~BoxGeometryComponent(void)
+	{
+		
+	}
+
+	void BoxGeometryComponent::RegisterReflection()
+	{
+		GASS::ComponentFactory::GetPtr()->Register("BoxGeometryComponent",new GASS::Creator<BoxGeometryComponent, IComponent>);
+		RegisterProperty<Vec3>("Size", &GASS::BoxGeometryComponent::GetSize, &GASS::BoxGeometryComponent::SetSize);
+	}
+
+	void BoxGeometryComponent::OnCreate()
+	{
+		GetSceneObject()->RegisterForMessage(typeid(LoadCoreComponentsMessage),MESSAGE_FUNC(BoxGeometryComponent::OnLoad),0);
+	}
+
+	Vec3 BoxGeometryComponent::GetSize() const
+	{
+		return m_Size;
+	}
+
+	void BoxGeometryComponent::SetSize(const Vec3 &value)
+	{
+		m_Size = value;
+	}
+	
+	void BoxGeometryComponent::OnLoad(MessagePtr message)
+	{
+
+		Vec3 size= m_Size*0.5;
+		ManualMeshDataPtr mesh_data(new ManualMeshData());
+		MeshVertex vertex;
+		mesh_data->Material = "WhiteTransparentNoLighting";
+
+		vertex.TexCoord.Set(0,0);
+		vertex.Color = Vec4(1,1,1,1);
+		mesh_data->Type = LINE_LIST;
+		std::vector<Vec3> conrners;
+
+		conrners.push_back(Vec3( size.x ,size.y , size.z));
+		conrners.push_back(Vec3(-size.x ,size.y , size.z));
+		conrners.push_back(Vec3(-size.x ,size.y ,-size.z));
+		conrners.push_back(Vec3( size.x ,size.y ,-size.z));
+
+		conrners.push_back(Vec3( size.x ,-size.y , size.z));
+		conrners.push_back(Vec3(-size.x ,-size.y , size.z));
+		conrners.push_back(Vec3(-size.x ,-size.y ,-size.z));
+		conrners.push_back(Vec3( size.x ,-size.y ,-size.z));
+
+		for(int i = 0; i < 4; i++)
+		{
+			vertex.Pos = conrners[i];
+			mesh_data->VertexVector.push_back(vertex);
+			vertex.Pos = conrners[(i+1)%4];
+			mesh_data->VertexVector.push_back(vertex);
+
+			vertex.Pos = conrners[i];
+			mesh_data->VertexVector.push_back(vertex);
+			vertex.Pos = conrners[i+4];
+			mesh_data->VertexVector.push_back(vertex);
+		}
+
+		for(int i = 0; i < 4; i++)
+		{
+			vertex.Pos = conrners[4 + i];
+			mesh_data->VertexVector.push_back(vertex);
+			vertex.Pos = conrners[4 + ((i+1)%4)];
+			mesh_data->VertexVector.push_back(vertex);
+		}
+		MessagePtr mesh_message(new ManualMeshDataMessage(mesh_data));
+		GetSceneObject()->PostMessage(mesh_message);
+	}
+}
---
+++ trunk/source/Plugins/Base/Components/BoxGeometryComponent.h
@@ -0,0 +1,40 @@
+
+/****************************************************************************
+*                                                                           *
+* HiFiEngine                                                                *
+* Copyright (C)2003 - 2005 Johan Hedstrom                                   *
+* Email: hifiengine@gmail.com                                               *
+* Web page: http://n00b.dyndns.org/HiFiEngine                               *
+*                                                                           *
+* HiFiEngine is only used with knowledge from the author. This software     *
+* is not allowed to redistribute without permission from the author.        *
+* For further license information, please turn to the product home page or  *
+* contact author. Abuse against the HiFiEngine license is prohibited by law.*
+*                                                                           *
+*****************************************************************************/ 
+#ifndef BOX_GEOMETRY_COMPONENT_H
+#define BOX_GEOMETRY_COMPONENT_H
+
+#include "Sim/Components/BaseSceneComponent.h"
+#include "Sim/Common.h"
+#include "Core/MessageSystem/IMessage.h"
+
+namespace GASS
+{
+	class BoxGeometryComponent : public Reflection<BoxGeometryComponent,BaseSceneComponent>
+	{
+	public:
+		BoxGeometryComponent(void);
+		~BoxGeometryComponent(void);
+		static void RegisterReflection();
+		virtual void OnCreate();
+	protected:
+		void OnLoad(MessagePtr message);
+		void OnChangeName(MessagePtr message);
+		Vec3 GetSize() const;
+		void SetSize(const Vec3 &value);
+		Vec3 m_Size;
+	private:
+	};
+}
+#endif
