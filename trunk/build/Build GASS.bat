71cffa04b19fbe31a52133fe81b733c6fec6bf52
b.lindahl@gmail.com <b.lindahl@gmail.com@b0bc8688-3f32-11de-b232-599c1b3c59e8>
2011-05-25 13:52:51 +0000

    Now build project GASSCore first and then the complete solution so new projects will be compiled without specifically add them to the build batch file.

--- trunk/build/Build GASS.bat
+++ trunk/build/Build GASS.bat
@@ -49,33 +49,57 @@ if exist "C:\Program Files\Xoreax\IncrediBuild\BuildConsole.exe". (
 )
 goto :EOF
 
+:COMPILESOLUTION
+Rem Parameters: <solutionname> <projectname> <configurationname> (</USEENV>)
+if exist "C:\Program Files\Xoreax\IncrediBuild\BuildConsole.exe". (
+		echo "Building using C:\Program Files\Xoreax\IncrediBuild\BuildConsole.exe" 
+	"C:\Program Files\Xoreax\IncrediBuild\BuildConsole.exe" %1 /build /cfg=%2 %3 >> %GASS_BUILDLOG%
+) else (
+	if exist "C:\Program Files (x86)\Xoreax\IncrediBuild\BuildConsole.exe". (
+		echo "Building using C:\Program Files (x86)\Xoreax\IncrediBuild\BuildConsole.exe"
+		"C:\Program Files (x86)\Xoreax\IncrediBuild\BuildConsole.exe" %1 /build /cfg=%2 %3 >> %GASS_BUILDLOG%
+	) else (
+		echo "Building using devenv.exe"
+		devenv /build %2 %1 %3 >> %GASS_BUILDLOG%
+	)
+)
+goto :EOF
+
 :BUILDGASS
 
 rem Build Debug
 call :COMPILE GASSSim.sln "GASSCore" "Debug|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSSim" "Debug|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSPluginOgre" "Debug|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSPluginODE" "Debug|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSPluginOIS" "Debug|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSPluginOpenAL" "Debug|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSPluginPagedGeometry" "Debug|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSPluginGame" "Debug|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSPluginBase" "Debug|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSPluginRakNet" "Debug|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSEditorModule" "Debug|Win32" /USEENV
+call :COMPILESOLUTION GASSSim.sln "Debug|Win32" /USEENV
+
+
+
+REM call :COMPILE GASSSim.sln "GASSSim" "Debug|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginOgre" "Debug|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginODE" "Debug|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginOIS" "Debug|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginOpenAL" "Debug|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginPagedGeometry" "Debug|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginGame" "Debug|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginBase" "Debug|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginRakNet" "Debug|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSEditorModule" "Debug|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginEnvironment" "Debug|Win32" /USEENV
 
 rem Build Release
 call :COMPILE GASSSim.sln "GASSCore" "Release|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSSim" "Release|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSPluginOgre" "Release|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSPluginODE" "Release|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSPluginOIS" "Release|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSPluginOpenAL" "Release|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSPluginPagedGeometry" "Release|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSPluginGame" "Release|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSPluginBase" "Release|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSPluginRakNet" "Release|Win32" /USEENV
-call :COMPILE GASSSim.sln "GASSEditorModule" "Release|Win32" /USEENV
+call :COMPILESOLUTION GASSSim.sln "Release|Win32" /USEENV
+
+REM call :COMPILE GASSSim.sln "GASSSim" "Release|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginOgre" "Release|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginODE" "Release|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginOIS" "Release|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginOpenAL" "Release|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginPagedGeometry" "Release|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginGame" "Release|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginBase" "Release|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginRakNet" "Release|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSEditorModule" "Release|Win32" /USEENV
+REM call :COMPILE GASSSim.sln "GASSPluginEnvironment" "Release|Win32" /USEENV
 
 pause
 cd ..\..
