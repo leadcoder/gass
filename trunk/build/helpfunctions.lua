6549d4f16da4f01d5a4bc77844bd21cdfb1dcc49
b.lindahl@gmail.com <b.lindahl@gmail.com@b0bc8688-3f32-11de-b232-599c1b3c59e8>
2011-09-01 06:55:38 +0000

    Added helper variable for ois lib folder structure.

--- trunk/build/helpfunctions.lua
+++ trunk/build/helpfunctions.lua
@@ -4,3 +4,10 @@ if (_ACTION == "vs2003") then tbverdir = "vc7.1"
 elseif (_ACTION == "vs2005") then tbverdir = "vc8"
 elseif (_ACTION == "vs2008") then tbverdir = "vc9"
 elseif (_ACTION == "codeblocks") then tbverdir = "cc4.1.0_libc2.4_kernel2.6.16.21"  end
+
+vsVersion = "";
+if (_ACTION == "vs2005") then vsVersion = "vs71"
+elseif (_ACTION == "vs2008") then vsVersion = "vs8"
+elseif (_ACTION == "vs2009") then vsVersion = "vs9"
+elseif (_ACTION == "vs2010") then vsVersion = "vs10"
+end
