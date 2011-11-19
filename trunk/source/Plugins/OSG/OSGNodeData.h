873464fbff48a9e59a8f66f1023d41cdb43dd629
leadcoder <leadcoder@b0bc8688-3f32-11de-b232-599c1b3c59e8>
2011-11-19 16:24:03 +0000

    node data class added

---
+++ trunk/source/Plugins/OSG/OSGNodeData.h
@@ -0,0 +1,35 @@
+/****************************************************************************
+* This file is part of GASS.                                                *
+* See http://code.google.com/p/gass/                                 *
+*                                                                           *
+* Copyright (c) 2008-2009 GASS team. See Contributors.txt for details.      *
+*                                                                           *
+* GASS is free software: you can redistribute it and/or modify              *
+* it under the terms of the GNU Lesser General Public License as published  *
+* by the Free Software Foundation, either version 3 of the License, or      *
+* (at your option) any later version.                                       *
+*                                                                           *
+* GASS is distributed in the hope that it will be useful,                   *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU Lesser General Public License for more details.                       *
+*                                                                           *
+* You should have received a copy of the GNU Lesser General Public License  *
+* along with GASS. If not, see <http://www.gnu.org/licenses/>.              *
+*****************************************************************************/
+#pragma once
+
+#include "Sim/Common.h"
+#include "Sim/Components/BaseSceneComponent.h"
+#include <osg/Referenced>
+namespace GASS
+{
+	class OSGNodeData : osg::Referenced
+	{
+	public:
+		OSGNodeData(BaseSceneComponentPtr  component):m_Component(component) {}
+		virtual ~OSGNodeData(){}
+		BaseSceneComponentWeakPtr m_Component;
+	};
+
+}
