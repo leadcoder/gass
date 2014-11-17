f33e873a3a0f58ea966825b57f6b015f3f77b6be
leadcoder@gmail.com <leadcoder@gmail.com@b0bc8688-3f32-11de-b232-599c1b3c59e8>
2014-11-17 08:17:09 +0000

    MISSING files added

---
+++ trunk/source/Plugins/OSG/Utils/CustomTerrainManipulator.cpp
@@ -0,0 +1,34 @@
+#include "CustomTerrainManipulator.h"
+#include <osg/Quat>
+#include <osg/Notify>
+#include <osg/BoundsChecking>
+
+#include <iostream>
+
+using namespace osg;
+using namespace osgGA;
+
+CustomTerrainManipulator::CustomTerrainManipulator() : TerrainManipulator()
+{
+	//_thrown = false;
+	//_distance = 1.0f;
+	//_trackballSize = 0.8f;
+}
+
+
+CustomTerrainManipulator::~CustomTerrainManipulator()
+{
+
+}
+
+bool CustomTerrainManipulator::performMovementRightMouseButton( const double eventTimeDelta, const double dx, const double dy )
+{
+	// zoom model
+	return TerrainManipulator::performMovementLeftMouseButton( eventTimeDelta, dx, dy );
+}
+
+bool CustomTerrainManipulator::performMovementLeftMouseButton( const double eventTimeDelta, const double dx, const double dy )
+{
+	return true;
+}
+
