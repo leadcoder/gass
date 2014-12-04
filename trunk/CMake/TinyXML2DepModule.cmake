b6c8e1558d046909bf24543376652130593ffb08
leadcoder@gmail.com <leadcoder@gmail.com@b0bc8688-3f32-11de-b232-599c1b3c59e8>
2014-12-04 12:05:15 +0000

    tinyxml2 added to cmake

---
+++ trunk/CMake/TinyXML2DepModule.cmake
@@ -0,0 +1,6 @@
+set(TINYXML2_INC_DIR ${GASS_DEPENDENCIES_DIR}/tinyxml2/include)
+set(TINYXML2_LIB_DIR ${GASS_DEPENDENCIES_DIR}/tinyxml2/lib)
+set(TINYXML2_LINK_LIBRARIES debug tinyxml2_d
+			optimized tinyxml2)
+
+
