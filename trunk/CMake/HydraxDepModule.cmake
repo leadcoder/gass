338bb418455c8291b8dbb1db965b8dbb929b8cdf
leadcoder@gmail.com <leadcoder@gmail.com@b0bc8688-3f32-11de-b232-599c1b3c59e8>
2015-02-02 15:34:27 +0000

    find_package continued

--- trunk/CMake/HydraxDepModule.cmake
+++ trunk/CMake/HydraxDepModule.cmake
@@ -1,7 +1,8 @@
-set(HYDRAX_DIR "${DEPENDENCIES_DIR}/Hydrax-v0.5.1" CACHE PATH "Hydrax folder")
+set(HYDRAX_DIR "${GASS_DEPENDENCIES_DIR}/Hydrax-v0.5.1" CACHE PATH "Hydrax folder")
+find_package(Hydrax)
 
-set(HYDRAX_INC_DIR ${HYDRAX_DIR}/include)
-set(HYDRAX_LIB_DIR ${HYDRAX_DIR}/lib)
+#set(HYDRAX_INC_DIR ${HYDRAX_DIR}/include)
+#set(HYDRAX_LIB_DIR ${HYDRAX_DIR}/lib)
 
-set(HYDRAX_LINK_LIBRARIES debug Hydrax_d
-			optimized Hydrax)
+#set(HYDRAX_LINK_LIBRARIES debug Hydrax_d
+#			optimized Hydrax)
--- trunk/CMake/SkyXDepModule.cmake
+++ trunk/CMake/SkyXDepModule.cmake
@@ -1,11 +1,13 @@
-set(SKYX_DIR  "${DEPENDENCIES_DIR}/SkyX-v0.4" CACHE PATH "SkyX folder")
+set(SKYX_DIR  "${GASS_DEPENDENCIES_DIR}/SkyX-v0.4" CACHE PATH "SkyX folder")
+
+find_package(SkyX)
 
 set(SKYX_BIN_DIR ${SKYX_DIR}/bin)
-set(SKYX_INC_DIR ${SKYX_DIR}/include/SKYX)
-set(SKYX_LIB_DIR ${SKYX_DIR}/lib)
+#set(SKYX_INC_DIR ${SKYX_DIR}/include/SKYX)
+#set(SKYX_LIB_DIR ${SKYX_DIR}/lib)
 
-set(SKYX_LINK_LIBRARIES debug SkyX_d
-			optimized SkyX)
+#set(SKYX_LINK_LIBRARIES debug SkyX_d
+#			optimized SkyX)
 
 set(SKYX_BIN_FILES_DEBUG ${SKYX_BIN_DIR}/debug/skyx_d.dll)
 set(SKYX_BIN_FILES_RELEASE ${SKYX_BIN_DIR}/release/skyx.dll)
