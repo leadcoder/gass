06fe88f5279c225c2e084514cad59f354bda95d1
leadcoder <leadcoder@gmailcom>
2016-08-14 20:57:03 +0200

    CMake: new targets commands introduced.

--- CMake/Common.cmake
+++ CMake/Common.cmake
@@ -104,3 +104,45 @@ install(TARGETS ${LIB_NAME}
 	
 endmacro(gass_install_plugin_target) 
 
+function(mkcmakeconfig packagename packageversion)
+
+include(CMakePackageConfigHelpers)
+write_basic_package_version_file(
+  "${CMAKE_CURRENT_BINARY_DIR}/${packagename}ConfigVersion.cmake"
+  VERSION ${packageversion}
+  COMPATIBILITY AnyNewerVersion
+)
+
+export(EXPORT LibraryTargets
+  FILE "${CMAKE_CURRENT_BINARY_DIR}/${packagename}Targets.cmake"
+  NAMESPACE ${packagename}::
+)
+configure_file(cmake/${packagename}Config.cmake
+  "${CMAKE_CURRENT_BINARY_DIR}/${packagename}Config.cmake"
+  COPYONLY
+)
+
+if(WIN32)
+  set(ConfigPackageLocation cmake)
+else()
+  set(ConfigPackageLocation lib/cmake/${packagename})
+endif()
+install(EXPORT LibraryTargets
+  FILE
+    ${packagename}Targets.cmake
+  NAMESPACE
+    ${packagename}::
+  DESTINATION
+    ${ConfigPackageLocation}
+)
+install(
+  FILES
+    cmake/${packagename}Config.cmake
+    "${CMAKE_CURRENT_BINARY_DIR}/${packagename}ConfigVersion.cmake"
+  DESTINATION
+    ${ConfigPackageLocation}
+  COMPONENT
+    Devel
+)
+endfunction()
+
