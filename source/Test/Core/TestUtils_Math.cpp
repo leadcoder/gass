6f1fb67ac08ebac031c2247dc828a2d94de8e4bb
leadcoder <leadcoder@gmail.com>
2017-11-08 13:07:36 +0100

    Test: Equal test added

--- source/Test/Core/TestUtils_Math.cpp
+++ source/Test/Core/TestUtils_Math.cpp
@@ -4,6 +4,19 @@
 
 TEST_CASE("Test Math")
 {
+
+	SECTION("Test Equal")
+	{
+		REQUIRE(GASS::Math::Equal(0.0f, 0.0f, 0.001f) == true);
+		REQUIRE(GASS::Math::Equal(0.0f, 1.0f, 0.001f) == false);
+		REQUIRE(GASS::Math::Equal(1.0f, 1.0011f, 0.001f) == false);
+		REQUIRE(GASS::Math::Equal(100000.0, 100000.0, 0.001) == true);
+		REQUIRE(GASS::Math::Equal(100000.0, 100000.0009, 0.001) == true);
+
+		double value = 123456789.123;
+		REQUIRE(GASS::Math::Equal(value, value, std::numeric_limits<double>::epsilon()) == true);
+	}
+
 	SECTION("Test Deg2Rad")
 	{
 		float radf = GASS::Math::Deg2Rad(180.0f);
