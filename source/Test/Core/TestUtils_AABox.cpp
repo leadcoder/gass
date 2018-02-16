98bdc750694324deffa0e8af7ff23630d4efc370
leadcoder <leadcoder@gmail.com>
2018-02-16 13:18:54 +0100

    Tests: AABox ray intersection test added

--- source/Test/Core/TestUtils_AABox.cpp
+++ source/Test/Core/TestUtils_AABox.cpp
@@ -19,8 +19,8 @@ TEST_CASE("Test AABox")
 		const GASS::Vec3 max(2, 2, 2);
 		const GASS::AABox box(min, max);
 		GASS::Float line_dist = 0;
-		GASS::LineSegment line(GASS::Vec3(1, 1, 4), GASS::Vec3(1, 1, 0));
-		bool hit = box.LineIntersect(line, line_dist);
+		const GASS::LineSegment line(GASS::Vec3(1, 1, 4), GASS::Vec3(1, 1, 0));
+		const bool hit = box.LineIntersect(line, line_dist);
 		REQUIRE(hit == true);
 		REQUIRE(0.5 == line_dist);
 		GASS::Vec3 isect_point = line.GetPoint(line_dist);
@@ -33,12 +33,12 @@ TEST_CASE("Test AABox")
 		const GASS::Vec3 max(2, 2, 2);
 		const GASS::AABox box(min, max);
 		GASS::Float line_dist = 0;
-		GASS::LineSegment line(GASS::Vec3(0, 0, 4), GASS::Vec3(0, 0, 0));
-		bool hit = box.LineIntersect(line, line_dist);
+		const GASS::LineSegment line(GASS::Vec3(0, 0, 4), GASS::Vec3(0, 0, 0));
+		const bool hit = box.LineIntersect(line, line_dist);
 		REQUIRE(hit == true);
 		REQUIRE(0.5 == line_dist);
 		GASS::Vec3 isect_point = line.GetPoint(line_dist);
-		REQUIRE(isect_point == GASS::Vec3(0, 0, 2));
+		REQUIRE(isect_point.Equal(GASS::Vec3(0, 0, 2)));
 	}
 
 	SECTION("Test LineIntersect inside box")
@@ -47,12 +47,12 @@ TEST_CASE("Test AABox")
 		const GASS::Vec3 max(2, 2, 2);
 		const GASS::AABox box(min, max);
 		GASS::Float line_dist = 0;
-		GASS::LineSegment line(GASS::Vec3(1, 1, 1), GASS::Vec3(0, 3, 0));
-		bool hit = box.LineIntersect(line, line_dist);
+		const GASS::LineSegment line(GASS::Vec3(1, 1, 1), GASS::Vec3(0, 3, 0));
+		const bool hit = box.LineIntersect(line, line_dist);
 		REQUIRE(hit == true);
 		REQUIRE(0 == line_dist);
 		GASS::Vec3 isect_point = line.GetPoint(line_dist);
-		REQUIRE(isect_point == GASS::Vec3(1, 1, 1));
+		REQUIRE(isect_point.Equal(GASS::Vec3(1, 1, 1)));
 	}
 
 	SECTION("Test LineIntersect on box side")
@@ -61,12 +61,67 @@ TEST_CASE("Test AABox")
 		const GASS::Vec3 max(2, 2, 2);
 		const GASS::AABox box(min, max);
 		GASS::Float line_dist = 0;
-		GASS::LineSegment line(GASS::Vec3(1, 1, 2), GASS::Vec3(1, 1, 3));
-		bool hit = box.LineIntersect(line, line_dist);
+		const GASS::LineSegment line(GASS::Vec3(1, 1, 2), GASS::Vec3(1, 1, 3));
+		const bool hit = box.LineIntersect(line, line_dist);
 		REQUIRE(hit == true);
 		REQUIRE(0 == line_dist);
-		GASS::Vec3 isect_point = line.GetPoint(line_dist);
-		REQUIRE(isect_point == GASS::Vec3(1, 1, 2));
+		const GASS::Vec3 isect_point = line.GetPoint(line_dist);
+		REQUIRE(isect_point.Equal(GASS::Vec3(1, 1, 2)));
 	}
 
+	SECTION("Test RayIntersect")
+	{
+		const GASS::Vec3 min(0, 0, 0);
+		const GASS::Vec3 max(2, 2, 2);
+		const GASS::AABox box(min, max);
+		GASS::Float near_dist = 0;
+		GASS::Float far_dist = 0;
+		const GASS::Ray ray(GASS::Vec3(1, 1, 4), GASS::Vec3(0, 0, -1));
+		const bool hit = box.RayIntersect(ray, near_dist, far_dist);
+		REQUIRE(hit == true);
+		REQUIRE(near_dist == 2);
+		REQUIRE(far_dist == 4);
+	}
+
+	SECTION("Test RayIntersect edge")
+	{
+		const GASS::Vec3 min(0, 0, 0);
+		const GASS::Vec3 max(2, 2, 2);
+		const GASS::AABox box(min, max);
+		GASS::Float near_dist = 0;
+		GASS::Float far_dist = 0;
+		const GASS::Ray ray(GASS::Vec3(0, 0, 4), GASS::Vec3(0, 0, -1));
+		const bool hit = box.RayIntersect(ray, near_dist, far_dist);
+		REQUIRE(hit == false);
+	}
+
+	SECTION("Test RayIntersect inside box")
+	{
+		const GASS::Vec3 min(0, 0, 0);
+		const GASS::Vec3 max(2, 2, 2);
+		const GASS::AABox box(min, max);
+		
+		GASS::Float near_dist = 0;
+		GASS::Float far_dist = 0;
+		const GASS::Ray ray(GASS::Vec3(1, 1, 1), GASS::Vec3(0, -1, 0));
+		const bool hit = box.RayIntersect(ray, near_dist, far_dist);
+		REQUIRE(hit == true);
+		REQUIRE(near_dist == -1);
+		REQUIRE(far_dist == 1);
+	}
+
+	SECTION("Test RayIntersect on box side")
+	{
+		const GASS::Vec3 min(0, 0, 0);
+		const GASS::Vec3 max(2, 2, 2);
+		const GASS::AABox box(min, max);
+		GASS::Float near_dist = 0;
+		GASS::Float far_dist = 0;
+		const GASS::Ray ray(GASS::Vec3(1, 1, 2), GASS::Vec3(0, 0, 1));
+		const bool hit = box.RayIntersect(ray, near_dist, far_dist);
+		REQUIRE(hit == false);
+	}
+
+
+	
 }
