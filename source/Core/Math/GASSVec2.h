70e7c6b65c21b6f4c9e06b35f6aef5a793dde82e
leadcoder <leadcoder@gmailcom>
2019-03-27 22:26:18 +0100

    More methods added to TVec2

--- source/Core/Math/GASSVec2.h
+++ source/Core/Math/GASSVec2.h
@@ -45,13 +45,14 @@ namespace GASS
 	class TVec2
 	{
 	public:
-		TYPE x,y;
-		inline TVec2(TYPE _x, TYPE _y)
+		TYPE x, y;
+		TVec2(TYPE _x, TYPE _y) : x(_x), y(_y)
 		{
-			x = _x;y = _y;
+
 		}
-		inline TVec2() = default;
-		inline void Set(TYPE _x, TYPE _y)
+		TVec2() = default;
+
+		void Set(TYPE _x, TYPE _y)
 		{
 			x = _x; y = _y;
 		}
@@ -63,6 +64,14 @@ namespace GASS
 			ret.y = y + v.y;
 			return ret;
 		}
+		
+		TVec2& operator += (const TVec2& v)
+		{
+			x += v.x;
+			y += v.y;
+			return *this;
+		}
+
 		TVec2 operator- (const TVec2 &v) const
 		{
 			TVec2 ret;
@@ -70,6 +79,7 @@ namespace GASS
 			ret.y = y - v.y;
 			return ret;
 		}
+
 		TVec2 operator- () const
 		{
 			TVec2 ret;
@@ -77,17 +87,128 @@ namespace GASS
 			ret.y = -y;
 			return ret;
 		}
+
+		TVec2& operator -= (const TVec2& v)
+		{
+			x -= v.x;
+			y -= v.y;
+			return *this;
+		}
+
 		TVec2 operator* (TYPE scalar) const
 		{
 			TVec2 ret;
-			ret.x = x*scalar;
-			ret.y = y*scalar;
+			ret.x = x * scalar;
+			ret.y = y * scalar;
 			return ret;
 		}
 
-		inline bool operator== (const TVec2 &v) const
+		TVec2& operator *= (const TYPE scalar)
 		{
-			return (v.x == x &&  v.y == y);
+			x *= scalar;
+			y *= scalar;
+			return *this;
+		}
+
+		friend TVec2 operator * (TYPE scalar, const TVec2& v)
+		{
+			TVec2 ret;
+			ret.x = scalar * v.x;
+			ret.y = scalar * v.y;
+			return ret;
+		}
+
+		TVec2& operator *= (const TVec2& v)
+		{
+			x *= v.x;
+			y *= v.y;
+			return *this;
+		}
+
+		TVec2 operator* (const TVec2 &v) const
+		{
+			TVec2 temp_v;
+			temp_v.x = x * v.x;
+			temp_v.y = y * v.y;
+			return temp_v;
+		}
+
+		TVec2 operator/ (const TYPE scalar) const
+		{
+			TVec2 temp_v;
+			temp_v.x = x / scalar;
+			temp_v.y = y / scalar;
+			return temp_v;
+		}
+
+		TVec2 operator/ (const TVec2 &v) const
+		{
+			TVec2 temp_v;
+			temp_v.x = x / v.x;
+			temp_v.y = y / v.y;
+			return temp_v;
+		}
+
+		bool operator== (const TVec2 &v) const
+		{
+			return (v.x == x && v.y == y);
+		}
+
+		bool operator!= (const TVec2 &v) const
+		{
+			return !(v.x == x && v.y == y);
+		}
+
+		TYPE& operator [] (unsigned element)
+		{
+			assert(element < 2);
+			return *(&x + element);
+		}
+
+		TYPE operator [] (unsigned element) const
+		{
+			assert(element < 2);
+			return *(&x + element);
+		}
+
+		bool Equal(const TVec2 &v, TYPE tolerance = std::numeric_limits<TYPE>::epsilon()) const
+		{
+			return Math::Equal(x, v.x, tolerance) && Math::Equal(y, v.y, tolerance);
+		}
+
+		TYPE SquaredLength() const
+		{
+			return x * x + y * y;
+		}
+
+		TYPE Length() const
+		{
+			return sqrt(x * x + y * y);
+		}
+
+		inline TYPE Normalize()
+		{
+			TYPE dist = Length();
+
+			if (dist > 0)
+			{
+				x /= dist;
+				y /= dist;
+			}
+			else
+			{
+				//consider 0, 0 instead or assert
+				x = 1;
+				y = 0;
+			}
+			return dist;
+		}
+
+		inline TVec2 NormalizedCopy() const
+		{
+			TVec2 norm_copy(x, y);
+			norm_copy.Normalize();
+			return norm_copy;
 		}
 
 		friend std::ostream& operator << (std::ostream& os, const TVec2& vec)
@@ -98,11 +219,34 @@ namespace GASS
 
 		friend std::istream& operator >> (std::istream& is, TVec2& vec)
 		{
-			if(!(is >> vec.x >>  vec.y))
+			if (!(is >> vec.x >> vec.y))
 			{
-				GASS_EXCEPT(Exception::ERR_INVALIDPARAMS, "Failed to parse vec2 value","Vec2::>>");
+				GASS_EXCEPT(Exception::ERR_INVALIDPARAMS, "Failed to parse vec2 value", "Vec2::>>");
 			}
 			return is;
+		}
+
+		static TVec2<double> Convert(const TVec2<float> &vf)
+		{
+			return TVec2<double>(vf.x, vf.y);
+		}
+
+		static TVec2<float> Convert(const TVec2<double> &vd)
+		{
+			return TVec2<float>(static_cast<float>(vd.x),
+				static_cast<float>(vd.y));
+		}
+
+		static void Convert(const TVec2<double> &vd, TVec2<float> &vf)
+		{
+			vf.x = static_cast<float>(vd.x);
+			vf.y = static_cast<float>(vd.y);
+		}
+
+		static void Convert(const TVec2<float> &vf, TVec2<double> &vd)
+		{
+			vd.x = vf.x;
+			vd.y = vf.y;
 		}
 	};
 
--- source/Test/Core/TestUtils_Vector.cpp
+++ source/Test/Core/TestUtils_Vector.cpp
@@ -2,6 +2,202 @@
 #include "Core/Math/GASSMath.h"
 #include "catch.hpp"
 
+
+TEST_CASE("Test Vec2")
+{
+	SECTION("Test constructor")
+	{
+		GASS::Vec2 vec(1, 1);
+		REQUIRE(vec.x == 1);
+		REQUIRE(vec.y == 1);
+	}
+
+	SECTION("Test convert , from Vec3d to Vec3f")
+	{
+		GASS::Vec2d vecd(1, 2);
+		GASS::Vec2f vecf = GASS::Vec2f::Convert(vecd);
+		REQUIRE(vecf.x == 1);
+		REQUIRE(vecf.y == 2);
+	}
+
+	SECTION("Test convert , from Vec3f to Vec3d")
+	{
+		GASS::Vec2d vecf(1, 2);
+		GASS::Vec2f vecd = GASS::Vec2d::Convert(vecf);
+		REQUIRE(vecd.x == 1);
+		REQUIRE(vecd.y == 2);
+
+		GASS::Vec2i vec2(1, 2);
+		GASS::Vec2i vec4(1, 2);
+		GASS::Vec2i vec3 = vec4 + vec2;
+		REQUIRE(vec3 == GASS::Vec2i(2, 4));
+	}
+
+	SECTION("Test set")
+	{
+		GASS::Vec2 vec;
+		vec.Set(1, 1);
+		REQUIRE(vec.x == 1);
+		REQUIRE(vec.y == 1);
+	}
+
+	SECTION("Test operator=")
+	{
+		GASS::Vec2 vec(1, 1);
+		REQUIRE(vec == GASS::Vec2(1, 1));
+	}
+
+	SECTION("Test Equal")
+	{
+		GASS::Vec2 vec1(1.1, 1.3);
+		GASS::Vec2 vec2(1.0, 1.0);
+
+		//test if in tolerance
+		REQUIRE(vec1.Equal(vec2, 0.4) == true);
+
+		//test if out of tolerance
+		REQUIRE(vec1.Equal(vec2, 0.2) == false);
+	}
+
+	SECTION("Test operator+")
+	{
+		GASS::Vec2 vec1(1, 1);
+		GASS::Vec2 vec2(2, 2);
+		GASS::Vec2 res = vec1 + vec2;
+		REQUIRE(res == GASS::Vec2(3, 3));
+	}
+
+	SECTION("Test operator +=")
+	{
+		GASS::Vec2 vec1(1, 1);
+		GASS::Vec2 vec2(2, 2);
+		vec1 += vec2;
+		REQUIRE(vec1 == GASS::Vec2(3, 3));
+	}
+
+	SECTION("Test operator- 1")
+	{
+		GASS::Vec2 vec1(2, 2);
+		GASS::Vec2 vec2(1, 1);
+		GASS::Vec2 res = vec1 - vec2;
+
+		REQUIRE(res == GASS::Vec2(1, 1));
+	}
+
+	SECTION("Test operator- 2")
+	{
+		GASS::Vec2 vec(1, 1);
+		GASS::Vec2 res = -vec;
+		REQUIRE(res == GASS::Vec2(-1, -1));
+	}
+
+	SECTION("Test operator -=")
+	{
+		GASS::Vec2 res(2, 2);
+		res -= GASS::Vec2(1, 1);
+		REQUIRE(res == GASS::Vec2(1, 1));
+	}
+
+	SECTION("Test operator* (scalar)")
+	{
+		GASS::Vec2 res = GASS::Vec2(2, 2) * 2;
+		REQUIRE(res == GASS::Vec2(4, 4));
+	}
+
+	SECTION("Test operator=* (scalar)")
+	{
+		GASS::Vec2 res(2, 2);
+		res *= 2;
+		REQUIRE(res == GASS::Vec2(4, 4));
+	}
+
+	SECTION("Test operator* (Vec2)")
+	{
+		GASS::Vec2 res = GASS::Vec2(2, 2) * GASS::Vec2(2, 1);
+		REQUIRE(res == GASS::Vec2(4, 2));
+	}
+
+	SECTION("Test operator=* (Vec2)")
+	{
+		GASS::Vec2 res(2, 2);
+		res *= GASS::Vec2(2, 1);
+		REQUIRE(res == GASS::Vec2(4, 2));
+	}
+
+	SECTION("Test operator/ (Vec2)")
+	{
+		GASS::Vec2 res = GASS::Vec2(2, 2) / GASS::Vec2(2, 1);
+		REQUIRE(res == GASS::Vec2(1, 2));
+	}
+
+	SECTION("Test operator/ (Scalar)")
+	{
+		GASS::Vec2 res = GASS::Vec2(2, 2) / 2;
+		REQUIRE(res == GASS::Vec2(1, 1));
+	}
+
+	SECTION("Test operator[] ")
+	{
+		GASS::Vec2 vec(2, 3);
+		REQUIRE(vec[0] == 2);
+		REQUIRE(vec[1] == 3);
+	}
+
+	SECTION("Test operator * (Type scalar, const TVec2& v) ")
+	{
+		GASS::Vec2 res = 2 * GASS::Vec2(2, 4);
+		REQUIRE(res == GASS::Vec2(4, 8));
+	}
+
+	SECTION("Test operator != ")
+	{
+		REQUIRE(GASS::Vec2(4, 2) != GASS::Vec2(4, 4));
+	}
+
+	SECTION("Test SquaredLength")
+	{
+		REQUIRE(GASS::Vec2(1, 2).SquaredLength() == 5);
+	}
+
+	SECTION("Test Length")
+	{
+		REQUIRE(GASS::Vec2(9, 0).Length() == 9);
+
+		REQUIRE(GASS::Vec2(0, 0).Length() == 0);
+	}
+
+	SECTION("Test Normalize")
+	{
+		GASS::Vec2 vec(2, 0);
+		vec.Normalize();
+		REQUIRE(vec == GASS::Vec2(1, 0));
+
+		//Test zero vector
+		vec.Set(0, 0);
+		vec.Normalize();
+		//for now 1,0,0 should be returned, consider 0,0,0 instead 
+		REQUIRE(vec == GASS::Vec2(1, 0));
+	}
+
+	SECTION("Test streaming <<")
+	{
+		std::stringstream ss;
+		GASS::Vec2 vec(2, 0);
+		ss << vec;
+		const std::string str_vec = ss.str();
+		REQUIRE(str_vec == "2 0");
+	}
+
+	SECTION("Test streaming >>")
+	{
+		std::stringstream ss;
+		ss << "2 0";
+		GASS::Vec2 vec;
+		ss >> vec;
+		REQUIRE(vec == GASS::Vec2(2, 0));
+	}
+}
+
 TEST_CASE("Test Vec3")
 {
 	SECTION("Test constructor")
@@ -45,6 +241,8 @@ TEST_CASE("Test Vec3")
 		REQUIRE(vec == GASS::Vec3(1, 1, 1));
 	}
 
+	
+
 	SECTION("Test Equal")
 	{
 		GASS::Vec3 vec1(1.1, 1.2, 1.3);
@@ -56,7 +254,6 @@ TEST_CASE("Test Vec3")
 		//test if out of tolerance
 		REQUIRE(vec1.Equal(vec2, 0.2) == false);
 	}
-
 	
 
 	SECTION("Test operator+")
