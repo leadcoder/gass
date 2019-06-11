24dd695bf16b0fe104e3e34e76ba9d7d1850afd6
leadcoder <leadcoder@gmailcom>
2019-06-11 18:45:53 +0200

    changed StringUtils from class to name space

--- source/Core/Utils/GASSStringUtils.cpp
+++ source/Core/Utils/GASSStringUtils.cpp
@@ -25,85 +25,148 @@
 
 #include <cctype>
 #ifndef _MSC_VER
-    #include <cxxabi.h>
+#include <cxxabi.h>
 #endif
 
 namespace GASS
 {
-	std::string StringUtils::RemoveQuotation(const char* str)
+	namespace StringUtils
 	{
-		std::string ret = str;
-		ret = ret.substr(1,ret.length()-2);
-		return ret;
-	}
-
-	std::string StringUtils::RemoveQuotation(const std::string &str)
-	{
-		std::string ret = str;
-		ret = ret.substr(1,ret.length()-2);
-		return ret;
-	}
-
-	unsigned char CharToUpper(char ch)
-	{
-		return static_cast<unsigned char>(std::toupper(static_cast<unsigned char>(ch)));
-	}
-
-	unsigned char CharToLower(char ch)
-	{
-		return static_cast<unsigned char>(std::tolower(static_cast<unsigned char>(ch)));
-	}
-
-	std::string StringUtils::ToUpper(const std::string&str)
-	{
-		std::string new_str = str;
-		std::transform(str.begin(), str.end(), new_str.begin(), CharToUpper);
-		return new_str;
-	}
-
-	std::string StringUtils::ToLower(const std::string &str)
-	{
-		std::string new_str = str;
-		std::transform(str.begin(), str.end(), new_str.begin(), CharToLower);
-		return new_str;
-	}
-
-	std::string StringUtils::Replace(const std::string &str, const std::string &find, const std::string &replacement)
-	{
-		std::string::size_type  pos;
-		std::string::size_type  look_here = 0;
-		std::string new_str = str;
-
-		// what we are going to replace already exist replacement string, throw to indicate this.
-		if (replacement.find(find) != std::string::npos)
-			GASS_EXCEPT(Exception::ERR_INTERNAL_ERROR, "String to replace with contains string searched for - will result in infinite loop!", "StringUtils::Replace");
-
-		while ((pos = new_str.find(find, look_here)) != std::string::npos)
+		std::string RemoveQuotation(const char* str)
 		{
-			new_str.replace(pos, find.size(), replacement);
-			look_here = pos + replacement.size();
+			std::string ret = str;
+			ret = ret.substr(1, ret.length() - 2);
+			return ret;
 		}
-		return new_str;
-	}
 
-	std::string StringUtils::Demangle(const std::string &name)
-	{
+		std::string RemoveQuotation(const std::string &str)
+		{
+			std::string ret = str;
+			ret = ret.substr(1, ret.length() - 2);
+			return ret;
+		}
+
+		unsigned char CharToUpper(char ch)
+		{
+			return static_cast<unsigned char>(std::toupper(static_cast<unsigned char>(ch)));
+		}
+
+		unsigned char CharToLower(char ch)
+		{
+			return static_cast<unsigned char>(std::tolower(static_cast<unsigned char>(ch)));
+		}
+
+		std::string ToUpper(const std::string&str)
+		{
+			std::string new_str = str;
+			std::transform(str.begin(), str.end(), new_str.begin(), CharToUpper);
+			return new_str;
+		}
+
+		std::string ToLower(const std::string &str)
+		{
+			std::string new_str = str;
+			std::transform(str.begin(), str.end(), new_str.begin(), CharToLower);
+			return new_str;
+		}
+
+		int CompareNoCase(const std::string &str1, const std::string &str2)
+		{
+			return ToLower(str1).compare(ToLower(str2));
+		}
+
+		std::string Replace(const std::string &str, const std::string &find, const std::string &replacement)
+		{
+			std::string::size_type  pos;
+			std::string::size_type  look_here = 0;
+			std::string new_str = str;
+
+			// what we are going to replace already exist replacement string, throw to indicate this.
+			if (replacement.find(find) != std::string::npos)
+				GASS_EXCEPT(Exception::ERR_INTERNAL_ERROR, "String to replace with contains string searched for - will result in infinite loop!", "StringUtils::Replace");
+
+			while ((pos = new_str.find(find, look_here)) != std::string::npos)
+			{
+				new_str.replace(pos, find.size(), replacement);
+				look_here = pos + replacement.size();
+			}
+			return new_str;
+		}
+
+		std::string Demangle(const std::string &name)
+		{
 #ifdef _MSC_VER
-		if(std::string::npos == name.find("class"))
-			return name;
-		std::string ret = name.substr(6);
+			if (std::string::npos == name.find("class"))
+				return name;
+			std::string ret = name.substr(6);
 #else
-		int status;
-		std::string ret = abi::__cxa_demangle(name.c_str(), 0, 0, &status);
+			int status;
+			std::string ret = abi::__cxa_demangle(name.c_str(), 0, 0, &status);
 #endif
-		//remove namespace
-		const size_t pos = ret.find("::");
-		if(pos != std::string::npos)
-		{
-			ret =  ret.substr(pos+2);
+			//remove namespace
+			const size_t pos = ret.find("::");
+			if (pos != std::string::npos)
+			{
+				ret = ret.substr(pos + 2);
+			}
+			return ret;
 		}
-		return ret;
+
+		//Specialized template implementation to catch std::string
+		template <>
+		bool GetValueFromString<std::string>(std::string &res, const std::string &s)
+		{
+			res = s;
+			return true;
+		}
+
+		//Specialized template implementation to catch bool
+		template <>
+		bool GetValueFromString<bool>(bool &res, const std::string &s)
+		{
+			std::stringstream ss;
+			if (s.find("0") == std::string::npos && s.find("1") == std::string::npos)
+				ss.setf(std::ios::boolalpha);
+			ss << s;
+			ss >> res;
+			return !ss.fail();
+		}
+
+		template <>
+		bool GetStringFromValue(const float &val, std::string &res)
+		{
+			std::stringstream ss;
+			// When converting to float/double, clamp values to max precision data type (digits10) can guarantee + 1 value digit.
+			ss << std::setprecision(std::numeric_limits<float>::digits10 + 1) << val;
+			res = ss.str();
+			return !ss.fail();
+		}
+
+		template <>
+		bool GetStringFromValue(const double &val, std::string &res)
+		{
+			std::stringstream ss;
+			ss << std::setprecision(std::numeric_limits<double>::digits10 + 1) << val;
+			res = ss.str();
+			return !ss.fail();
+		}
+
+		template <>
+		bool GetStringFromValue(const std::vector<float> &val, std::string &res)
+		{
+			std::stringstream ss;
+			ss << std::setprecision(std::numeric_limits<float>::digits10 + 1) << val;
+			res = ss.str();
+			return !ss.fail();
+		}
+
+		template <>
+		bool GetStringFromValue(const std::vector<double> &val, std::string &res)
+		{
+			std::stringstream ss;
+			ss << std::setprecision(std::numeric_limits<double>::digits10 + 1) << val;
+			res = ss.str();
+			return !ss.fail();
+		}
 	}
-
-
 }
--- source/Core/Utils/GASSStringUtils.h
+++ source/Core/Utils/GASSStringUtils.h
@@ -38,21 +38,21 @@ namespace GASS
 	Class used for string operations
 	*/
 
-	class GASSCoreExport StringUtils
+	namespace StringUtils
 	{
-	public:
+	
 		/**
 		@brief Return first and last character in string (actually not checking if quotation characters).
 		@param str String to work on.
 		@return String without first and last character.
 		*/
-		static std::string RemoveQuotation(const char* str);
+		GASSCoreExport std::string RemoveQuotation(const char* str);
 		/**
 		@brief Return first and last character in string (actually not checking if quotation characters).
 		@param str String to work on.
 		@return String without first and last character.
 		*/
-		static std::string RemoveQuotation(const std::string &str);
+		GASSCoreExport std::string RemoveQuotation(const std::string &str);
 		/**
 		@brief Replace all occurances of find in str with replacement string.
 		@param str String to look in.
@@ -60,46 +60,45 @@ namespace GASS
 		@param replacement String to replace all occurrences of find with.
 		@return String str but with all occurrences of find string replaced by replacement string.
 		*/
-		static std::string Replace(const std::string &str, const std::string &find, const std::string &replacement);
+		GASSCoreExport std::string Replace(const std::string &str, const std::string &find, const std::string &replacement);
 
 		/**
 		@brief Return string in str with all characters converted to lower case.
 		@param str String to work on.
 		@return All lower case string.
 		*/
-		static std::string ToLower(const std::string &str);
+		GASSCoreExport std::string ToLower(const std::string &str);
 		/**
 		@brief Return string in str with all characters converted to upper case.
 		@param str String to work on.
 		@return All upper case string.
 		*/
-		static std::string ToUpper(const std::string&str);
+		GASSCoreExport std::string ToUpper(const std::string&str);
 
-		static int CompareNoCase(const std::string &str1, const std::string &str2)
-		{
-			return StringUtils::ToLower(str1).compare(ToLower(str2));
-		}
-		static std::string Demangle(const std::string &name);
+		GASSCoreExport int CompareNoCase(const std::string &str1, const std::string &str2);
+		
+		GASSCoreExport std::string Demangle(const std::string &name);
 
 		template <class TYPE>
-		static bool GetValueFromString(TYPE &res, const std::string &s)
+		bool GetValueFromString(TYPE &res, const std::string &s)
 		{
 			std::stringstream ss(s);
 			ss >> res;
 			return !ss.fail();
 		}
 
-		//Use specialized template to catch std::string
+		//Specialized template implementation to catch std::string
 		template <>
-		static bool GetValueFromString<std::string>(std::string &res, const std::string &s);
+		GASSCoreExport bool GetValueFromString<std::string>(std::string &res, const std::string &s);
+		
 
-		//Use specialized template to catch bool
+		//Specialized template implementation to catch bool
 		template <>
-		static bool GetValueFromString<bool>(bool &res, const std::string &s);
-
+		GASSCoreExport bool GetValueFromString<bool>(bool &res, const std::string &s);
+	
 		//-------------------------Get string from value---------------------
 		template <class TYPE>
-		static bool GetStringFromValue(const TYPE &val, std::string &res)
+		bool GetStringFromValue(const TYPE &val, std::string &res)
 		{
 			std::stringstream sstream;
 
@@ -113,76 +112,19 @@ namespace GASS
 
 		//Use specialized template to catch float
 		template <>
-		static bool GetStringFromValue<float>(const float &val, std::string &res);
+		GASSCoreExport bool GetStringFromValue<float>(const float &val, std::string &res);
 
 		//Use specialized template to catch double
 		template <>
-		static bool GetStringFromValue<double>(const double &val, std::string &res);
+		GASSCoreExport bool GetStringFromValue<double>(const double &val, std::string &res);
 
 		//Use specialized template to catch float vector
 		template <>
-		static bool GetStringFromValue< std::vector<float> >(const std::vector<float> &val, std::string &res);
+		GASSCoreExport bool GetStringFromValue< std::vector<float> >(const std::vector<float> &val, std::string &res);
 
 		//Use specialized template to catch double vector
 		template <>
-		static bool GetStringFromValue< std::vector<double> >(const std::vector<double> &val, std::string &res);
-	};
-
-	//Specialized template implementation to catch std::string
-	template <>
-	bool StringUtils::GetValueFromString<std::string>(std::string &res, const std::string &s)
-	{
-		res = s;
-		return true;
-	}
-
-	//Specialized template implementation to catch bool
-	template <>
-	bool StringUtils::GetValueFromString<bool>(bool &res, const std::string &s)
-	{
-		std::stringstream ss;
-		if (s.find("0") == std::string::npos && s.find("1") == std::string::npos)
-			ss.setf(std::ios::boolalpha);
-		ss << s;
-		ss >> res;
-		return !ss.fail();
-	}
-
-	template <>
-	bool StringUtils::GetStringFromValue(const float &val, std::string &res)
-	{
-		std::stringstream ss;
-		// When converting to float/double, clamp values to max precision data type (digits10) can guarantee + 1 value digit.
-		ss << std::setprecision(std::numeric_limits<float>::digits10 + 1) << val;
-		res = ss.str();
-		return !ss.fail();
-	}
-
-	template <>
-	bool StringUtils::GetStringFromValue(const double &val, std::string &res)
-	{
-		std::stringstream ss;
-		ss << std::setprecision(std::numeric_limits<double>::digits10 + 1) << val;
-		res = ss.str();
-		return !ss.fail();
-	}
-
-	template <>
-	bool StringUtils::GetStringFromValue(const std::vector<float> &val, std::string &res)
-	{
-		std::stringstream ss;
-		ss << std::setprecision(std::numeric_limits<float>::digits10 + 1) << val;
-		res = ss.str();
-		return !ss.fail();
-	}
-
-	template <>
-	bool StringUtils::GetStringFromValue(const std::vector<double> &val, std::string &res)
-	{
-		std::stringstream ss;
-		ss << std::setprecision(std::numeric_limits<double>::digits10 + 1) << val;
-		res = ss.str();
-		return !ss.fail();
+		GASSCoreExport bool GetStringFromValue< std::vector<double> >(const std::vector<double> &val, std::string &res);
 	}
 }
 
