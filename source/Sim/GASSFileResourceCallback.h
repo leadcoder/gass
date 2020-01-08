bfd32cf5372dd0781807e3eeb5ad431ef09f27e7
Johan <leadcoder@gmail.com>
2020-01-08 09:56:35 +0100

    Fix some compiler issues

--- source/Sim/GASSFileResourceCallback.h
+++ source/Sim/GASSFileResourceCallback.h
@@ -18,35 +18,52 @@
 * along with GASS. If not, see <http://www.gnu.org/licenses/>.              *
 *****************************************************************************/
 
-#ifndef GASS_FILE_RESOURCE_H
-#define GASS_FILE_RESOURCE_H
-
+#pragma once
 #include "Sim/GASSCommon.h"
 #include "Core/Utils/GASSFilePath.h"
 #include "Core/Reflection/GASSIProperty.h"
 #include "Sim/GASSResourceLocation.h"
+#include "Sim/GASSResourceManager.h"
+#include "Sim/GASSResourceGroup.h"
+#include "Sim/GASSResourceLocation.h"
+
 
 namespace GASS
 {
-	class GASSExport FileResource
+	template <class TYPE>
+	class FileResourceCallback : public IPropertyOptionsCallback<TYPE>
 	{
 	public:
-		FileResource(const FilePath &path,ResourceLocationPtr location, const std::string &type) : m_ResourcePath(path),
-			m_Location(location),
-			m_ResourceType(type)
+		FileResourceCallback(std::string res_group, std::string res_type): m_ResourceGroup(res_group),
+			m_ResourceType(res_type)
 		{
 
 		}
-		
-		std::string Name() const { return m_ResourcePath.GetFilename();}
-		ResourceLocationPtr Location() const { return m_Location;}
-		std::string Type() const { return m_ResourceType;}
-		FilePath Path() const { return m_ResourcePath;}
+		std::vector<TYPE> GetEnumeration() const override
+		{
+			std::vector<TYPE> content;
+			ResourceManagerPtr rm = SimEngine::Get().GetResourceManager();
+			ResourceGroupVector groups = rm->GetResourceGroups();
+			std::vector<std::string> values;
+			for (size_t i = 0; i < groups.size(); i++)
+			{
+				GASS::ResourceGroupPtr group = groups[i];
+				if (m_ResourceGroup == "" || group->GetName() == m_ResourceGroup)
+				{
+					ResourceVector res_vec;
+					group->GetResourcesByType(res_vec, m_ResourceType);
+					for (size_t j = 0; j < res_vec.size(); j++)
+					{
+						content.push_back(res_vec[j]->Name());
+					}
+				}
+			}
+			return content;
+		}
 	private:
-		FilePath m_ResourcePath;
-		ResourceLocationPtr m_Location;
+		std::string m_ResourceGroup;
 		std::string m_ResourceType;
 	};
-	GASS_PTR_DECL(FileResource)
+	//typedef GASS_SHARED_PTR<FileResourceCallback<TYPE> > FileResourceCallbackPtr;
 }
-#endif 
+
