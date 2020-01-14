f0f7d7589a88bfb328538799caf922cae064fff8
leadcoder <leadcoder@gmailcom>
2020-01-14 23:20:19 +0100

    cmake: add missing files

---
+++ CMake/GASSSimConfig.cmake
@@ -0,0 +1,4 @@
+get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+include(${SELF_DIR}/AngelscriptTargets.cmake)
+include(${SELF_DIR}/GASSCoreTargets.cmake)
+include(${SELF_DIR}/GASSSimTargets.cmake)
