26c5c68e7fcc6b9dedd9ac69905ad2fd71723456
leadcoder <leadcoder@gmailcom>
2020-01-15 15:50:32 +0100

    cmake: use single config file

--- CMake/GASSSimConfig.cmake
+++
@@ -1,4 +0,0 @@
-get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/AngelscriptTargets.cmake)
-include(${SELF_DIR}/GASSCoreTargets.cmake)
-include(${SELF_DIR}/GASSSimTargets.cmake)
