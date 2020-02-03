6da4fa44eeab7846d3b5dac32efa9dfc5ef1aed9
leadcoder <leadcoder@gmail.com>
2020-02-03 13:12:07 +0100

    cmake: Add lowecase support for config

--- CMake/GASSConfig.cmake
+++ CMake/GASSConfig.cmake
@@ -11,12 +11,19 @@ ENDMACRO(LIST_CONTAINS)
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(${SELF_DIR}/GASSCoreTargets.cmake)
 
-LIST_CONTAINS(USE_SIM Sim ${GASS_FIND_COMPONENTS})
 
+set(GASS_COMPONENT_LIST ${GASS_FIND_COMPONENTS})
+
+#handle lowercase version ie find_package(gass ...) 
+if(NOT GASS_COMPONENT_LIST AND gass_FIND_COMPONENTS)
+	set(GASS_COMPONENT_LIST  ${gass_FIND_COMPONENTS})
+endif()
+
+LIST_CONTAINS(USE_SIM Sim ${GASS_COMPONENT_LIST})
 if(USE_SIM)
 	include(${SELF_DIR}/AngelscriptTargets.cmake)
 	include(${SELF_DIR}/GASSSimTargets.cmake)
-	LIST_CONTAINS(USE_EDITOR_MODULE EditorModule ${GASS_FIND_COMPONENTS})
+	LIST_CONTAINS(USE_EDITOR_MODULE EditorModule ${GASS_COMPONENT_LIST})
 	if (USE_EDITOR_MODULE)
 		include(${SELF_DIR}/GASSEditorModuleTargets.cmake)
 	endif()
