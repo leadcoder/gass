a6366e982ea1f03416afd55d0cf92808a9dd5c90
leadcoder <leadcoder@gmailcom>
2021-05-28 21:00:34 +0200

    remove old scripts

--- tools/deprecated/create_all_user_dep.bat
+++
@@ -1,6 +0,0 @@
-call deploy_user_dep.bat MSVC2010_x86
-call deploy_user_dep.bat MSVC2010_x64
-call deploy_user_dep.bat MSVC2012_x86
-call deploy_user_dep.bat MSVC2012_x64
-
-pause
--- tools/pack_core_dependecies.bat
+++
@@ -1,5 +0,0 @@
-7za.exe a gass-core-dependencies.7z %BOOST_ROOT% ^
-%TBB_INSTALL_DIR% ^
-%TINYXML2_HOME% ^
-prepare-cmake-environment.bat
-pause
